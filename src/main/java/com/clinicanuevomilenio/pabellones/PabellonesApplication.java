74ef66db43c3942e01e84135c21529b76cb0f9eb
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-23 22:12:31 -0400

    Primer commit Api Pabellones

---
+++ src/main/java/com/clinicanuevomilenio/pabellones/PabellonesApplication.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.pabellones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabellonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabellonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicanuevomilenio/pabellones/PabellonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.pabellones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabellonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
